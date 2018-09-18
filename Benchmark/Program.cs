6c081f42e453e8d55e787cf6aeb28d56ce6ce3c8
Mutuduxf <aeondxf@live.com>
2018-09-18 15:10:40 +0800

    Complete benchmark

---
+++ Benchmark/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using Zaabee.Jil;
+using Zaabee.Json;
+using Zaabee.Protobuf;
+using Zaabee.Xml;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BenchMarkTest>();
+            Console.ReadLine();
+        }
+    }
+
+    [SimpleJob(RunStrategy.ColdStart, targetCount: 10000)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class BenchMarkTest
+    {
+        private List<TestModel> _testModels;
+        private readonly string _jil;
+        private readonly string _json;
+        private readonly byte[] _protobuf;
+        private readonly string _xml;
+
+        public BenchMarkTest()
+        {
+            InitTestModel();
+            _jil = _testModels.ToJil();
+            _json = _testModels.ToJson();
+            _protobuf = _testModels.ToProtobuf();
+            _xml = _testModels.ToXml();
+        }
+
+        [Benchmark]
+        public void JilSerialize()
+        {
+            var jil = _testModels.ToJil();
+        }
+
+        [Benchmark]
+        public void JsonSerialize()
+        {
+            var json = _testModels.ToJson();
+        }
+
+        [Benchmark]
+        public void ProtobufSerialize()
+        {
+            var protobuf = _testModels.ToProtobuf();
+        }
+
+        [Benchmark]
+        public void XmlSerialize()
+        {
+            var xml = _testModels.ToXml();
+        }
+
+        [Benchmark]
+        public void JilDeserialize()
+        {
+            var model = _jil.FromJil<List<TestModel>>();
+        }
+
+        [Benchmark]
+        public void JsonDeserialize()
+        {
+            var model = _json.FromJson<List<TestModel>>();
+        }
+
+        [Benchmark]
+        public void ProtobufDeserialize()
+        {
+            var model = _protobuf.FromProtobuf<List<TestModel>>();
+        }
+
+        [Benchmark]
+        public void XmlDeserialize()
+        {
+            var model = _xml.FromXml<List<TestModel>>();
+        }
+
+        private void InitTestModel()
+        {
+            _testModels = new List<TestModel>
+            {
+                new TestModel
+                {
+                    Id = Guid.NewGuid(),
+                    Age = new Random().Next(0, 100),
+                    CreateTime = new DateTime(2017, 1, 1),
+                    Name = "apple",
+                    Gender = Gender.Female,
+                    Kids = new List<TestModel>
+                    {
+                        new TestModel
+                        {
+                            Id = Guid.NewGuid(),
+                            Age = new Random().Next(0, 100),
+                            CreateTime = new DateTime(2017, 1, 1),
+                            Name = "pear",
+                            Gender = Gender.Female
+                        },
+                        new TestModel
+                        {
+                            Id = Guid.NewGuid(),
+                            Age = new Random().Next(0, 100),
+                            CreateTime = new DateTime(2017, 1, 1),
+                            Name = "pear",
+                            Gender = Gender.Female
+                        },
+                        new TestModel
+                        {
+                            Id = Guid.NewGuid(),
+                            Age = new Random().Next(0, 100),
+                            CreateTime = new DateTime(2017, 1, 1),
+                            Name = "pear",
+                            Gender = Gender.Female
+                        },
+                        new TestModel
+                        {
+                            Id = Guid.NewGuid(),
+                            Age = new Random().Next(0, 100),
+                            CreateTime = new DateTime(2017, 1, 1),
+                            Name = "pear",
+                            Gender = Gender.Female
+                        },
+                        new TestModel
+                        {
+                            Id = Guid.NewGuid(),
+                            Age = new Random().Next(0, 100),
+                            CreateTime = new DateTime(2017, 1, 1),
+                            Name = "pear",
+                            Gender = Gender.Female
+                        }
+                    }
+                }
+            };
+        }
+    }
+
+    public class TestModel
+    {
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+        public Gender Gender { get; set; }
+        public List<TestModel> Kids { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
--- README.md
+++ README.md
@@ -64,3 +64,25 @@ var deserializeModel2 = xml.FromXml(typeof(TestModel)) as TestModel;
 var xml2 = testModel.ToXml(Encoding.UTF32);
 var deserializeModel3 = xml.FromXml<TestModel>(Encoding.UTF32);
 ```
+
+## Benchmark
+
+BenchmarkDotNet=v0.11.1, OS=Windows 10.0.17134.254 (1803/April2018Update/Redstone4)
+Intel Core i7-6600U CPU 2.60GHz (Max: 0.80GHz) (Skylake), 1 CPU, 4 logical and 2 physical cores
+Frequency=2742190 Hz, Resolution=364.6720 ns, Timer=TSC
+.NET Core SDK=2.1.402
+  [Host]     : .NET Core 2.1.4 (CoreCLR 4.6.26814.03, CoreFX 4.6.26814.02), 64bit RyuJIT  [AttachedDebugger]
+  Job-RUVWTX : .NET Core 2.1.4 (CoreCLR 4.6.26814.03, CoreFX 4.6.26814.02), 64bit RyuJIT
+
+IterationCount=10000  RunStrategy=ColdStart
+
+              Method |      Mean |      Error |       StdDev |    Median |        Min |          Max |
+-------------------- |----------:|-----------:|-------------:|----------:|-----------:|-------------:|
+        JilSerialize |  17.61 us |  0.3236 us |     9.830 us |  17.50 us |   7.658 us |     790.2 us |
+       JsonSerialize |  45.03 us |  1.2070 us |    36.672 us |  41.57 us |  23.704 us |   3,296.6 us |
+   ProtobufSerialize |  25.01 us |  0.3693 us |    11.221 us |  24.43 us |  11.305 us |     572.5 us |
+        XmlSerialize | 193.82 us |  1.7405 us |    52.879 us | 181.61 us | 103.567 us |     847.9 us |
+      JilDeserialize |  53.80 us | 97.4957 us | 2,962.045 us |  25.16 us |  12.034 us | 296,227.8 us |
+     JsonDeserialize |  87.58 us | 26.9408 us |   818.497 us |  71.11 us |  46.678 us |  81,872.2 us |
+ ProtobufDeserialize |  41.58 us |  6.3883 us |   194.086 us |  37.93 us |  19.328 us |  19,274.7 us |
+      XmlDeserialize | 239.83 us |  7.4513 us |   226.379 us | 213.70 us | 134.929 us |  21,493.0 us |
