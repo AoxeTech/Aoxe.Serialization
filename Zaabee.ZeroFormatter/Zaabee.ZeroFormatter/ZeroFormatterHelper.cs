4269d9bf969342f7ecebc6b5c7d5316cf93ded5a
Mutuduxf <aeondxf@live.com>
2019-08-28 11:00:34 +0800

    Recode zeroFormatter

--- Zaabee.ZeroFormatter/Zaabee.ZeroFormatter/ZeroFormatterHelper.cs
+++ Zaabee.ZeroFormatter/Zaabee.ZeroFormatter/ZeroFormatterHelper.cs
@@ -6,11 +6,6 @@ namespace Zaabee.ZeroFormatter
 {
     public static class ZeroFormatterHelper
     {
-        /// <summary>
-        /// Serialize the object to byte[](if the generic object == null then return new byte[0])
-        /// </summary>
-        /// <param name="t">generic object</param>
-        /// <returns>bytes</returns>
         public static byte[] Serialize<T>(T t) =>
             t is null ? new byte[0] : ZeroFormatterSerializer.Serialize(t);
 
@@ -42,13 +37,6 @@ namespace Zaabee.ZeroFormatter
         {
             if (obj != null) ZeroFormatterSerializer.NonGeneric.Serialize(type, stream, obj);
         }
-
-        /// <summary>
-        /// Deserialize the byte[] to the generic object(if the bytes is null or its length equals 0 then return default(T))
-        /// </summary>
-        /// <typeparam name="T">generic</typeparam>
-        /// <param name="bytes">bytes</param>
-        /// <returns>generic object</returns>
         public static T Deserialize<T>(byte[] bytes) =>
             bytes is null || bytes.Length == 0 ? default(T) : ZeroFormatterSerializer.Deserialize<T>(bytes);
 
