cb88e8301171f2afb516f2578dc7ebea8a5f1661
Mutuduxf <aeondxf@live.com>
2020-01-30 11:39:07 +0800

    Recode

--- tests/ZaabeeJilTestProject/TestModel.cs
+++ tests/ZaabeeJilTestProject/TestModel.cs
@@ -8,7 +8,6 @@ namespace ZaabeeJilTestProject
         public int Age { get; set; }
         public string Name { get; set; }
         public DateTimeOffset CreateTime { get; set; }
-
         public Gender Gender { get; set; }
     }
 }
