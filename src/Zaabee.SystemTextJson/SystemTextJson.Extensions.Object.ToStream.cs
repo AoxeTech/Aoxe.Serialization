a241056470b3be32025d09f14f0a550fbfd3ce45
XiaoFeiDu <romanchaos@163.com>
2021-12-10 16:49:08 +0800

    Modify return type

--- src/Zaabee.SystemTextJson/SystemTextJson.Extensions.Object.ToStream.cs
+++ src/Zaabee.SystemTextJson/SystemTextJson.Extensions.Object.ToStream.cs
@@ -2,9 +2,9 @@ namespace Zaabee.SystemTextJson;
 
 public static partial class SystemTextJsonExtensions
 {
-    public static Stream ToStream<TValue>(this TValue? value, JsonSerializerOptions? options = null) =>
+    public static MemoryStream ToStream<TValue>(this TValue? value, JsonSerializerOptions? options = null) =>
         SystemTextJsonHelper.ToStream(value, options);
 
-    public static Stream ToStream(this object? value, Type type, JsonSerializerOptions? options = null) =>
+    public static MemoryStream ToStream(this object? value, Type type, JsonSerializerOptions? options = null) =>
         SystemTextJsonHelper.ToStream(type, value, options);
 }
