989de1935945e41c58616d42cf55b97697518735
XiaoFeiDu <romanchaos@163.com>
2021-12-23 17:44:15 +0800

    ImplicitUsings enable

--- src/Zaabee.DataContractSerializer/GlobalUsings.cs
+++ src/Zaabee.DataContractSerializer/GlobalUsings.cs
@@ -1,8 +1,6 @@
-global using System;
-global using System.IO;
+global using System.Collections.Concurrent;
 global using System.Runtime.Serialization;
+global using System.Text;
+global using System.Xml;
 global using Zaabee.Serializer.Abstractions;
 global using Zaabee.Extensions;
-global using System.Xml;
-global using System.Collections.Concurrent;
-global using System.Text;
