35b7b446141ab34225fc1c516e7558c14dcaa79a
Mutudu <aeondxf@live.com>
2023-01-30 11:04:39 +0800

    Code refactoring.

--- src/Zaabee.MessagePack/MessagePack.Helper.Stream.FromStream.Async.cs
+++ src/Zaabee.MessagePack/MessagePack.Helper.Stream.FromStream.Async.cs
@@ -5,7 +5,7 @@ public static partial class MessagePackHelper
     public static async ValueTask<TValue?> FromStreamAsync<TValue>(Stream? stream,
         MessagePackSerializerOptions? options = null, CancellationToken cancellationToken = default)
     {
-        if (stream is null || stream.CanSeek && stream.Length is 0) return default;
+        if (stream is null or { CanSeek: true, Length: 0 }) return default;
         var result = await MessagePackSerializer.DeserializeAsync<TValue>(stream, options, cancellationToken);
         stream.TrySeek(0, SeekOrigin.Begin);
         return result;
@@ -14,7 +14,7 @@ public static partial class MessagePackHelper
     public static async ValueTask<object?> FromStreamAsync(Type type, Stream? stream,
         MessagePackSerializerOptions? options = null, CancellationToken cancellationToken = default)
     {
-        if (stream is null || stream.CanSeek && stream.Length is 0) return default;
+        if (stream is null or { CanSeek: true, Length: 0 }) return default;
         var result = await MessagePackSerializer.DeserializeAsync(type, stream, options, cancellationToken);
         stream.TrySeek(0, SeekOrigin.Begin);
         return result;
--- src/Zaabee.MessagePack/MessagePack.Helper.Stream.FromStream.cs
+++ src/Zaabee.MessagePack/MessagePack.Helper.Stream.FromStream.cs
@@ -5,7 +5,7 @@ public static partial class MessagePackHelper
     public static TValue? FromStream<TValue>(Stream? stream, MessagePackSerializerOptions? options = null,
         CancellationToken cancellationToken = default)
     {
-        if (stream is null || stream.CanSeek && stream.Length is 0) return default;
+        if (stream is null or { CanSeek: true, Length: 0 }) return default;
         var result = MessagePackSerializer.Deserialize<TValue>(stream, options, cancellationToken);
         stream.TrySeek(0, SeekOrigin.Begin);
         return result;
@@ -14,7 +14,7 @@ public static partial class MessagePackHelper
     public static object? FromStream(Type type, Stream? stream, MessagePackSerializerOptions? options = null,
         CancellationToken cancellationToken = default)
     {
-        if (stream is null || stream.CanSeek && stream.Length is 0) return default;
+        if (stream is null or { CanSeek: true, Length: 0 }) return default;
         var result = MessagePackSerializer.Deserialize(type, stream, options, cancellationToken);
         stream.TrySeek(0, SeekOrigin.Begin);
         return result;
