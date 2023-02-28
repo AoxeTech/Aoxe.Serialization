0b76db54f82204b864abdfd34c756a41502188eb
Mutudu <aeondxf@live.com>
2023-02-28 18:19:24 +0800

    Remove with length processing

--- src/Zaabee.Protobuf/Protobuf.Helper.Stream.FromStream.cs
+++ src/Zaabee.Protobuf/Protobuf.Helper.Stream.FromStream.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-
 namespace Zaabee.Protobuf;
 
 public static partial class ProtobufHelper
@@ -28,44 +26,6 @@ public static partial class ProtobufHelper
     {
         if (stream is null or { CanSeek: true, Length: 0 }) return default;
         var result = TypeModel.Deserialize(stream, null, type);
-        stream.TrySeek(0, SeekOrigin.Begin);
-        return result;
-    }
-
-    /// <summary>
-    /// Deserialize the stream to an object with a length-prefix.
-    /// </summary>
-    /// <param name="stream"></param>
-    /// <param name="prefixStyle"></param>
-    /// <param name="fieldNumber"></param>
-    /// <returns></returns>
-    public static object? FromStreamWithLengthPrefix<TValue>(
-        Stream? stream,
-        PrefixStyle prefixStyle = PrefixStyle.Base128,
-        int fieldNumber = 0)
-    {
-        if (stream is null or { CanSeek: true, Length: 0 }) return default;
-        var result = TypeModel.DeserializeWithLengthPrefix(stream, null, typeof(TValue), prefixStyle, fieldNumber);
-        stream.TrySeek(0, SeekOrigin.Begin);
-        return result;
-    }
-
-    /// <summary>
-    /// Deserialize the stream to an object with a length-prefix.
-    /// </summary>
-    /// <param name="type"></param>
-    /// <param name="stream"></param>
-    /// <param name="prefixStyle"></param>
-    /// <param name="fieldNumber"></param>
-    /// <returns></returns>
-    public static object? FromStreamWithLengthPrefix(
-        Type type,
-        Stream? stream,
-        PrefixStyle prefixStyle = PrefixStyle.Base128,
-        int fieldNumber = 0)
-    {
-        if (stream is null or { CanSeek: true, Length: 0 }) return default;
-        var result = TypeModel.DeserializeWithLengthPrefix(stream, null, type, prefixStyle, fieldNumber);
         stream.TrySeek(0, SeekOrigin.Begin);
         return result;
     }
