84b2d8708733f3cc1962a75fe06db0b79d4b5a20
Mutudu <aeondxf@live.com>
2023-09-07 15:05:19 +0800

    Remove data table tests

--- tests/Types.UnitTest/GlobalUsings.cs
+++ tests/Types.UnitTest/GlobalUsings.cs
@@ -1,8 +1,5 @@
 // Global using directives
 
-global using System.Data;
 global using Jil;
 global using Zaabee.Serializer.Abstractions;
 global using Xunit;
-global using TestModels;
-global using Zaabee.Extensions;
