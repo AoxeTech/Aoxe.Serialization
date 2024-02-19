08f28be5f4d2a6dfadfaccd384930587e9617949
Mutudu <aeondxf@live.com>
2024-02-20 02:57:56 +0800

    Dev

--- tests/TestModels/TestModel.cs
+++ tests/TestModels/TestModel.cs
@@ -9,22 +9,15 @@
 [ZeroFormattable]
 public partial class TestModel
 {
-    [Key(0)]
-    [ProtoMember(1)]
-    [Index(0)]
-    public virtual int Id { get; set; }
+    [Key(0)] [ProtoMember(1)] [Index(0)] public virtual int Id { get; set; }
 
-    [Key(1)]
-    [ProtoMember(2)]
-    [Index(1)]
-    public virtual int Age { get; set; }
+    [Key(1)] [ProtoMember(2)] [Index(1)] public virtual int Age { get; set; }
 
-    [Key(2)]
-    [ProtoMember(3)]
-    [Index(2)]
-    public virtual string? Name { get; set; }
+    [Key(2)] [ProtoMember(3)] [Index(2)] public virtual string? Name { get; set; }
 
-    public TestModel() { }
+    public TestModel()
+    {
+    }
 
     public override bool Equals(object? obj)
     {
@@ -38,5 +31,16 @@ public partial class TestModel
 
 #if !NETSTANDARD2_0
     public override int GetHashCode() => HashCode.Combine(Id, Age, Name);
+#else
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = Id;
+            hashCode = (hashCode * 397) ^ Age;
+            hashCode = (hashCode * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+            return hashCode;
+        }
+    }
 #endif
 }
