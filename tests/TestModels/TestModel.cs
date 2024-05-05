8d1999405106787f527ace831ab677fb81ff90e0
Mutudu <aeondxf@live.com>
2024-05-05 23:49:44 +0800

    Format codes

--- tests/TestModels/TestModel.cs
+++ tests/TestModels/TestModel.cs
@@ -9,15 +9,22 @@
 [ZeroFormattable]
 public partial class TestModel
 {
-    [Key(0)] [ProtoMember(1)] [Index(0)] public virtual int Id { get; set; }
+    [Key(0)]
+    [ProtoMember(1)]
+    [Index(0)]
+    public virtual int Id { get; set; }
 
-    [Key(1)] [ProtoMember(2)] [Index(1)] public virtual int Age { get; set; }
+    [Key(1)]
+    [ProtoMember(2)]
+    [Index(1)]
+    public virtual int Age { get; set; }
 
-    [Key(2)] [ProtoMember(3)] [Index(2)] public virtual string? Name { get; set; }
+    [Key(2)]
+    [ProtoMember(3)]
+    [Index(2)]
+    public virtual string? Name { get; set; }
 
-    public TestModel()
-    {
-    }
+    public TestModel() { }
 
     public override bool Equals(object? obj)
     {
